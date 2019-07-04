d6d4385e906d440b5200046bdc079ac2cf600d7a
Bin Yang <bin.yang@sbr.aldebaran.com>
2019-07-04 20:00:04 +0900

    remove rsyslogd start from bash file

--- run.sh
+++ run.sh
@@ -7,7 +7,6 @@ chown -R vagrant:vagrant /home/vagrant/.ssh/authorized_keys; chmod 600 /home/vag
 mkdir -p /root/.ssh
 echo $PUB_KEY >> /root/.ssh/authorized_keys
 chown -R root:root /root/.ssh/authorized_keys; chmod 600 /root/.ssh/authorized_keys
-echo '[ -f /usr/sbin/rsyslogd ] && /usr/sbin/rsyslogd' >> /home/vagrant/.bash_profile
 sed -i -e '/$ModLoad imjournal/s/^/#/' /etc/rsyslog.conf
 sed -i -e 's/$OmitLocalLogging on/$OmitLocalLogging off/' /etc/rsyslog.conf
 sed -i -e '/$IMJournalStateFile/s/^/#/' /etc/rsyslog.conf
